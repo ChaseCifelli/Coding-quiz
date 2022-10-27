3968e36b46b2ea97535bd6b49bd2e11cf2c7cddc
Chase Cifelli <chasecifelli@gmail.com>
2022-10-26 18:17:02 -0700

    added timer

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,6 +2,8 @@
 var startbutton = document.querySelector("#start");
 var startpage = document.querySelector("#startpage");
 var questionpage = document.querySelector("#questionpage");
+var secondsLeft = 10;
+var timeEl = document.querySelector("#time")
 //var count = localStorage.getItem("count");
 
 //counter.textContent = count;
@@ -9,7 +11,21 @@ var questionpage = document.querySelector("#questionpage");
 startbutton.addEventListener("click", function() {
  startpage.style.display="none";
  questionpage.style.display="block";
-
+ setTime();
 });
+function setTime() {
+  // Sets interval in variable
+  var timerInterval = setInterval(function() {
+    secondsLeft--;
+    timeEl.textContent = secondsLeft;
+
+    if(secondsLeft === 0) {
+      // Stops execution of action at set interval
+      clearInterval(timerInterval);
+      ;
+    }
+
+  }, 1000);
+}
 
 
